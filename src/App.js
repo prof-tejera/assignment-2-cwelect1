6dad37ff83616bc5503901365e2b5f35dcf038f8
Corbett Weinberg <cweinberg@hbs.edu>
2022-11-14 22:34:26 -0500

    removed extra BrowserRouter import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import styled from "styled-components";
 
 import DocumentationView from "./views/DocumentationView";
